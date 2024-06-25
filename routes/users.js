e39a927df99ddcb2869daf9192ed6621b7e6f721
LaurenJourney <161356914+LaurenJourney@users.noreply.github.com>
2024-06-25 11:48:10 +0100

    Dynamic routes

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,17 @@ router.get('/', (req, res) => {
 	res.send('User List');
 })
 
+router.post('/', (req, res) => {
+	res.send('Create user');
+})
 
 router.get('/new', (req, res) => {
 	res.send('New user form');
+})
+
+// put dynamic ones last, otherwise never reach specific ones
+router.get('/:id', (req, res) => {
+	res.send(`Get the User with ID ${req.params.id}`)
 })
 
 module.exports = router;
