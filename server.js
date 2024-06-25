db65edd33357231ea2a3d190b8f01c739021e650
LaurenJourney <161356914+LaurenJourney@users.noreply.github.com>
2024-06-25 11:32:22 +0100

    Start ejs server, plus install view engine ejs

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log('Init')
+const express = require('express');
+const app = express();
+
+// required to render .ejs files from a folder called "views"
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+	console.log('here');
+	
+	// Used mainly for testing 
+	// res.send('I got it');
+	
+	// res.status(200).json({ message: 'A thing happened' });
+
+	// res.download('server.js');
+
+	// Most common use case will be to render a html file or sending json
+	
+	// Render: 
+	// by default all views live under folder Views, so it must be created
+	// requires a VIEW ENGINE, e.g. ejs, pug
+	res.render('index');
+
+})
+
+app.listen(1234);
