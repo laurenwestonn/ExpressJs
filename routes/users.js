e9f095d756abe188bfce36c16b92f7aa60757d88
LaurenJourney <161356914+LaurenJourney@users.noreply.github.com>
2024-06-25 12:27:03 +0100

    Refactor to do CRUD on same route

--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,25 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 	res.send('User List');
-})
-
-router.post('/', (req, res) => {
-	res.send('Create user');
-})
-
-router.put('/', (req, res) => {
-	res.send('Create user');
-})
-
-router.delete('/', (req, res) => {
-	res.send('Create user');
-})
+});
 
 router.get('/new', (req, res) => {
 	res.send('New user form');
-})
+});
 
 // put dynamic ones last, otherwise never reach specific ones
-router.get('/:id', (req, res) => {
+router.route('/:id')
+.get((req, res) => {
 	res.send(`Get the User with ID ${req.params.id}`)
 })
+.put((req, res) => {
+	res.send(`Update the User with ID ${req.params.id}`)
+})
+.post((req, res) => {
+	res.send(`Create User with ID ${req.params.id}`)
+})
+.delete((req, res) => {
+	res.send(`Delete User ${req.params.id}`)
+});
 
 module.exports = router;
