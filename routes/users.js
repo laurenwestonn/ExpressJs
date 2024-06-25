cbd135b579268afa16028ca2a81fe7ba8da4d170
LaurenJourney <161356914+LaurenJourney@users.noreply.github.com>
2024-06-25 12:35:57 +0100

    Set values in the middleware

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [ { name: 'Taylor Swift' }, { name: 'Hayley Williams'} ];
+
 router.get('/', (req, res) => {
 	res.send('User List');
 });
@@ -12,6 +14,7 @@ router.get('/new', (req, res) => {
 // put dynamic ones last, otherwise never reach specific ones
 router.route('/:id')
 .get((req, res) => {
+	console.log(req.user.name); // this was set in the params middleware
 	res.send(`Get the User with ID ${req.params.id}`)
 })
 .put((req, res) => {
@@ -26,8 +29,9 @@ router.route('/:id')
 
 // Middleware. Happens after request, and before response
 // To say you're done, you must call the next thing explicitly
+// next() will call the res in this case
 router.param('id', (req, res, next, id) => {
-	console.log('hi');
+	req.user = users[id]; // can use this in any id response now! C, R, U and D. Don't have to set it multiple times!
 	next();
 });
 
