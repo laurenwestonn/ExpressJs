940f2aca0d3b6a6387c0eca93ba9679fc4a7acce
LaurenJourney <161356914+LaurenJourney@users.noreply.github.com>
2024-06-25 12:41:57 +0100

    Cleaning out comments to separate area
    Still useful for learning, but messy for current focus

--- server.js
+++ server.js
@@ -3,26 +3,40 @@ const app = express();
 
 // required to render .ejs files from a folder called "views"
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-	console.log('here');
-	
-	// Used mainly for testing 
-	// res.send('I got it');
-	
-	// res.status(200).json({ message: 'A thing happened' });
 
-	// res.download('server.js');
 
-	// Most common use case will be to render a html file or sending json
-	
-	// Render: 
-	// by default all views live under folder Views, so it must be created
-	// requires a VIEW ENGINE, e.g. ejs, pug
-	res.render('index', { text: 'World' });	// 2nd params allows you to send from server to FE
-
+app.get('/', (req, res) => {	
+	res.render('index', { text: 'World' });
 })
 
 const userRouter = require('./routes/users');
 app.use('/users', userRouter);
 
 app.listen(1234);
+
+
+
+
+
+
+// Exapmles of things you can do:
+// app.get('/', (req, res) => {	
+	//// Most common use case will be to render a html file or sending json
+	
+	// render: 
+	// only renders stuff inside of "views" folder, so make one!
+	// requires a VIEW ENGINE, e.g. ejs, pug
+	// res.render('index', { text: 'World' });	// 2nd params allows you to send from server to FE
+
+	//// Other things you can do
+
+	// send: Used mainly for testing 
+	// res.send('I got it');
+	
+	// statusCode: (or status if you want to return a message. Can be text or JSON)
+	// res.status(200).json({ message: 'A thing happened' });
+
+	// download: visiting url makes you download whatever you specify
+	// res.download('server.js');
+
+// })
